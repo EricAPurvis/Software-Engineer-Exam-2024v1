e7b469f782ee4d296618fc75f93559b1ed2896db
EricAPurvis <143045432+EricAPurvis@users.noreply.github.com>
2024-08-14 23:00:45 -0600

    Code Style Change
    Changed code formatting style to better fit requested formatting

--- vue-app/javascript/locationAPI.js
+++ vue-app/javascript/locationAPI.js
@@ -5,18 +5,18 @@ const geoApiKey="50edaa2484a34805b8e371b5434919f9";
 const geoApiUrl="https://api.geoapify.com/v2/places?categories=commercial&filter=geometry:48bfcbab59fa3867ed872c6e66649d8b";
 
 //Using lat and lon; gets the limit amount of commercial locations.
-async function getPointsOfInterest(lat, lon, limit){
+async function getPointsOfInterest(lat, lon, limit) {
 	
-	var requestOptions = {
-		method: 'GET',
-	};
+  var requestOptions = {
+    method: 'GET',
+  };
 
-	var poiData = [];
-	await fetch(`${geoApiUrl}&bias=proximity:${lon},${lat}&lang=en&limit=${limit}&apiKey=${geoApiKey}`, requestOptions)
-		.then(response => response.json())
-		.then(result => poiData=result)
-		.catch(error => console.log('error', error));
-		  
-	return(poiData);
+  var poiData = [];
+  await fetch(`${geoApiUrl}&bias=proximity:${lon},${lat}&lang=en&limit=${limit}&apiKey=${geoApiKey}`, requestOptions)
+    .then(response => response.json())
+    .then(result => poiData=result)
+	.catch(error => console.log('error', error));
+
+  return(poiData);
 
 }
--- vue-app/javascript/weatherAPI.js
+++ vue-app/javascript/weatherAPI.js
@@ -5,123 +5,125 @@ const apiKey = "0bdf710c6a3db9261f2e714e09c06ea8";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 	
 //Gathers and returns specified weather data
-async function getWeather(city, units){
-	const find = apiUrl + `q=${city}` + `&appid=${apiKey}` + `&units=${units}`;
-	const response = await fetch(find);
-	var data = await response.json();
+async function getWeather(city, units) {
+  const find = apiUrl + `q=${city}` + `&appid=${apiKey}` + `&units=${units}`;
+  const response = await fetch(find);
+  var data = await response.json();
+	
+  //Updates weather image
+  const imgName=data.weather[0].main.toLowerCase();
+  document.querySelector(".weather-icon").src = "images/"+`${imgName}`+".png";
 		
-	//Updates weather image
-	const imgName=data.weather[0].main.toLowerCase();
-	document.querySelector(".weather-icon").src = "images/"+`${imgName}`+".png";
+  //Uses Lat and Lon data to get points of interest
+  var poiData = getPointsOfInterest(data.coord.lat, data.coord.lon, 25);
 		
-	//Uses Lat and Lon data to get points of interest
-	var poiData = getPointsOfInterest(data.coord.lat, data.coord.lon, 25);
-		
-	//Returns collected weather data and poi
-	return([data, units, poiData]);
+  //Returns collected weather data and poi
+  return([data, units, poiData]);
 }
 	
 //Gets the specified city's weather and in the chosen units of measurement
-async function getSpecificWeather(){
-	const city = document.querySelector(".search #city").value;
-	const units = document.querySelector(".search #units").value;
-	return(getWeather(city, units));
+async function getSpecificWeather() {
+  const city = document.querySelector(".search #city").value;
+  const units = document.querySelector(".search #units").value;
+  return(getWeather(city, units));
 }
 	
 //Initial weather data getting
-async function getInitWeather(){
-	const city = "Tokyo";
-	const units = "Metric";
-	return(getWeather(city, units));
+async function getInitWeather() {
+  const city = "Tokyo";
+  const units = "Metric";
+  return(getWeather(city, units));
 }
 	
 //Vue dynamic data and method handling
-const app = Vue.createApp(
-	{
-		data(){
-			return{
-				city: "Japan",
-				temp: 100,
-				humidity: 100,
-				windSpeed: 100,
-				tempUnits: "°c",
-				speedUnits: "kmph",
-				poiData: ['1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5'],
-				currentPage:1,
-				maxPage:5,
-				pagination: 5
-			}
-		},
-		methods: {
-			//Moves the pagination state
-			movePage(increment){
-				this.currentPage+=increment;
-				if(this.currentPage>this.maxPage){this.currentPage=this.maxPage};
-				if(this.currentPage<1){this.currentPage=1};
-			},
-			//Opens a new tab with Google maps of the location(s) availble
-			googleMaps(index){
-				const poi=this.poiData[index];
-				const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${poi}+${this.city}+Japan`;
-				window.open(googleMapsURL);
-			},
-			//Updates POI based on selected location
-			async updatePOI(data){
-				for(index=0;index<this.poiData.length; index++){
-					this.poiData.splice(index, 1, "Loading");
-				}
-				
-				var poiData=[];
-				await data.then(function(result){
-					try{
-						poiData = result.features;
-					}catch(err){
-						console.log(err.message);
-					};
-				});
-				for(index=0; index<poiData.length; index++){
-					this.poiData[index]=poiData[index].properties.address_line1;
-				}
-				this.currentPage=1;
-			},
-			//Modifies the existing waether data to match new selections
-			async updateWeather(){
-				var data = await getSpecificWeather();
-				var units = data[1];
-					
-				const displayTempUnit = units=="Metric" ? "°c" : "°f";
-				const displaySpeedUnit = units=="Metric" ? " kmph" : " mph";
-					
-				this.city=data[0].name;
-				this.temp=Math.round(data[0].main.temp);
-				this.humidity=data[0].main.humidity;
-				this.windSpeed=data[0].wind.speed;
-				this.tempUnits=displayTempUnit;
-				this.speedUnits=displaySpeedUnit;
-					
-				this.updatePOI(data[2]);
-				
-			}
-		},
-		//Initializes the data for the user with the first options
-		created: async function(){
-				var data = await getInitWeather();
-				var units = data[1];
-					
-				const displayTempUnit = units=="Metric" ? "°c" : "°f";
-				const displaySpeedUnit = units=="Metric" ? " kmph" : " mph";
-					
-				this.city=data[0].name;
-				this.temp=Math.round(data[0].main.temp);
-				this.humidity=data[0].main.humidity;
-				this.windSpeed=data[0].wind.speed;
-				this.tempUnits=displayTempUnit;
-				this.speedUnits=displaySpeedUnit;
-					
-				this.updatePOI(data[2]);
-		}
+const app = Vue.createApp( {
+  data() {
+    return {
+	  city: "Japan",
+	  temp: 100,
+	  humidity: 100,
+	  windSpeed: 100,
+	  tempUnits: "°c",
+	  speedUnits: "kmph",
+	  poiData: ['1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5'],
+	  currentPage:1,
+	  maxPage:5,
+	  pagination: 5
 	}
-);
+  },
+  methods: {
+    //Moves the pagination state
+	movePage(increment) {
+	  this.currentPage+=increment;
+	  if(this.currentPage>this.maxPage) {
+	    this.currentPage=this.maxPage;
+	  }
+	  if(this.currentPage<1) {
+		this.currentPage=1;
+	  }
+    },
+    //Opens a new tab with Google maps of the location(s) availble
+    googleMaps(index) {
+      const poi=this.poiData[index];
+      const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${poi}+${this.city}+Japan`;
+      window.open(googleMapsURL);
+    },
+    //Updates POI based on selected location
+    async updatePOI(data) {
+      for(index=0;index<this.poiData.length; index++) {
+	    this.poiData.splice(index, 1, "Loading");
+	  }
+				
+	  var poiData=[];
+	  await data.then(function(result) {
+	    try {
+	      poiData = result.features;
+	    }catch(err) {
+	     console.log(err.message);
+	    };
+	  });
+      for(index=0; index<poiData.length; index++){
+	    this.poiData[index]=poiData[index].properties.address_line1;
+	  }
+	  this.currentPage=1;
+	},
+	//Modifies the existing waether data to match new selections
+	async updateWeather() {
+	  var data = await getSpecificWeather();
+	  var units = data[1];
+					
+	  const displayTempUnit = units=="Metric" ? "°c" : "°f";
+	  const displaySpeedUnit = units=="Metric" ? " kmph" : " mph";
+					
+	  this.city=data[0].name;
+	  this.temp=Math.round(data[0].main.temp);
+	  this.humidity=data[0].main.humidity;
+	  this.windSpeed=data[0].wind.speed;
+	  this.tempUnits=displayTempUnit;
+	  this.speedUnits=displaySpeedUnit;
+					
+	  this.updatePOI(data[2]);
+				
+	}
+  },
+  //Initializes the data for the user with the first options
+  created: async function() {
+    var data = await getInitWeather();
+	var units = data[1];
+					
+	const displayTempUnit = units=="Metric" ? "°c" : "°f";
+	const displaySpeedUnit = units=="Metric" ? " kmph" : " mph";
+					
+	this.city=data[0].name;
+	this.temp=Math.round(data[0].main.temp);
+	this.humidity=data[0].main.humidity;
+	this.windSpeed=data[0].wind.speed;
+	this.tempUnits=displayTempUnit;
+	this.speedUnits=displaySpeedUnit;
+					
+	this.updatePOI(data[2]);
+  }
+});
 
 //Mounts the VueJS app to the 'everything' class
 app.mount('.everything');
